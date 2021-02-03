9157820870eeb9c71bd9a694b2187f88c1adfbc3
Michael Ross <michaelross27@gmail.com>
2021-02-02 22:10:17 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission({item}) {
     return (
@@ -17,13 +24,6 @@ function Mission({item}) {
 
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
-
     static navigationOptions = {
         title: 'About Us'
     }
@@ -35,7 +35,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -46,15 +46,15 @@ class About extends Component {
                 <Card
                 title= "Community Partners"
             >
-                <FlatList
-                data={this.state.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
-            </Card>
+                <FlatList 
+                    data={this.props.partners.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                />
+                </Card>
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
